bae7c7444b8f9d4b65be9c15039d689e6ce468ce
Yining Wang <yining.wang@utexas.edu>
2019-12-11 10:49:10 -0600

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let button = document.querySelector("input#b");
+  let description = document.querySelector("input#new-task-description");
+  let list = document.querySelector("ul#tasks");
+
+  description.addEventListener("input", (event) => {
+    description.textContent = event.target.value;
+  });
+
+  button.addEventListener("click", (event) => {
+    let new_task = document.createElement('li');
+    let xbutton = document.createElement('button')
+    xbutton.textContent = "x";
+    new_task.textContent = description.textContent;
+    new_task.appendChild(xbutton);
+    list.appendChild(new_task);
+    xbutton.addEventListener("click", (event) => {
+      xbutton.parentNode.parentNode.removeChild(xbutton.parentNode);
+      event.preventDefault();
+    });
+    event.preventDefault();
+  });
 });
